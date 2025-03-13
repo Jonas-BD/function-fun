b742e1dbc0b7c13455f415c50170e19680d1c48e
Jonas-BD <joda12@elev.techcollege.dk>
2025-03-13 09:40:26 +0100

    Refactor helloWorld function to improve structure and readability

--- assets/js/functionsOne.js
+++ assets/js/functionsOne.js
@@ -1,9 +1,8 @@
 
 // her kaldes, eller invokes din funktion
-function helloWorld() {
-    console.log("Hello World");
-}
-
 helloWorld();
 
 // her skal du skrive din funktion
+function helloWorld() {
+    console.log("Hello World");
+}
--- assets/js/functionsTwo.js
+++ assets/js/functionsTwo.js
@@ -1,10 +1,9 @@
 
 
 // her kaldes, eller invokes din funktion
-function helloWorld(minValue, minAndenValue) {
-    console.log(minValue, minAndenValue);
-}
-
 helloWorld("hello world, med parameter", "1");
 
 // her skal du skrive din funktion
+function helloWorld(minValue, minAndenValue) {
+    console.log(minValue, minAndenValue);
+}
